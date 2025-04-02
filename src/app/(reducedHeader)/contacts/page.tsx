0213cf815d561d688272eb2b156ef8724c0034dc
Christopher Kaiser <christopher.kaiser@kleerly.de>
2025-04-03 00:22:02 +0200

    Readable Contacts

--- src/app/(reducedHeader)/contacts/page.tsx
+++ src/app/(reducedHeader)/contacts/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
             {config.contacts.map((contact, index) => (
                 <button
                     key={index}
-                    className={`w-full mb-2 p-4 text-third rounded drop-shadow-md bg-${contact.color}`}
+                    className={`w-full mb-2 p-4 text-primary-border font-bold rounded drop-shadow-md bg-${contact.color}`}
                     onClick={() => handleContactClick(contact)}
                 >
                     {contact.name}
